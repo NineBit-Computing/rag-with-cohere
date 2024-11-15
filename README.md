ae1ac8c700e8c703783c6fbd4fbfa5f1f393e215
Rajeev J <rajeev.jamaiyar@ninebit.in>
2024-11-15 16:49:36 +0530

    WIP: fast api implementation

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ User Cohere's APIs to demo RAG concept
 ## Install dependencies
 
 `pip install -r requirements.txt`
+
+## Dependecy snapshot
+
+`pip freeze > requirements.txt`
--- app.py
+++ app.py
@@ -1,12 +1,74 @@
+""" Implement RAG with Cohere """
+import json
+from typing import List
+from fastapi import FastAPI, HTTPException, Body, Request
+
 import cohere
+import numpy as np
 
-co = cohere.Client(
-    api_key="",
+
+# documents = [
+#     {
+#         "data": {
+#             "text": "Reimbursing Travel Expenses: Easily manage your travel expenses by submitting them through our finance tool. Approvals are prompt and straightforward."
+#         }
+#     },
+#     {
+#         "data": {
+#             "text": "Working from Abroad: Working remotely from another country is possible. Simply coordinate with your manager and ensure your availability during core hours."
+#         }
+#     },
+#     {
+#         "data": {
+#             "text": "Health and Wellness Benefits: We care about your well-being and offer gym memberships, on-site yoga classes, and comprehensive health insurance."
+#         }
+#     }
+# ]
+
+
+co = cohere.ClientV2(
+    api_key="UWrufrRl3IP6IR3T8iV4JzB5EZof8rpwEVm0Nz5G",
 )
 
-chat = co.chat(
-    message="hello world!",
-    model="command"
-)
+# Add the user query
+# query = "Are there health benefits?"
+# query = "Is it possible to work remotely?"
 
-print(chat)
+# Generate the response
+# response = co.chat(model="command-r-plus-08-2024",
+#                    messages=[{'role': 'user', 'content': query}], documents=documents)
+
+
+# chat = co.chat(
+#     message="hello world!",
+#     model="command"
+# )
+
+# print(chat)
+# print(response.message.content[0].text)
+
+# if response.message.citations:
+#     print("\nCITATIONS:")
+#     for citation in response.message.citations:
+#         print(citation, "\n")
+
+
+app = FastAPI()
+
+
+@app.post("/basic-rag/")
+async def handlerag(request: Request):
+    reqBody = await request.json()
+    reqDocuments = reqBody['documents']
+    reqQuery = reqBody['query']
+
+    response = co.chat(model="command-r-plus-08-2024",
+                       messages=[{'role': 'user', 'content': reqQuery}], documents=reqDocuments)
+
+    return response.message.content
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8001)
